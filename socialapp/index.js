6370e662d85864e476a057f67d7d6ec349a78e6e
Aakash Rawat <01tripster@gmail.com>
2022-07-29 20:07:34 +0530

    section 2 till video 11

---
+++ socialapp/index.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const format = require("date-format");
+
+const app = express();
+
+const PORT = 4000 || process.env.PORT;
+
+app.get("/", (req, res) => {
+	res.status(200).send("<h1>Hello from Ash</h1>");
+});
+
+app.get("/api/v1/instagram", (req, res) => {
+	const instaSocial = {
+		username: "aakashrawatInstapage",
+		followers: 69,
+		follows: 70,
+		date: format.asString("dd[MM] -- hh:mm:ss", new Date()),
+	};
+
+	res.status(200).json({ instaSocial });
+});
+
+app.get("/api/v1/facebook", (req, res) => {
+	const instaSocial = {
+		username: "aakashrawatFacebookpage",
+		followers: 69,
+		follows: 70,
+		date: format.asString("dd[MM] -- hh:mm:ss", new Date()),
+	};
+
+	res.status(200).json({ instaSocial });
+});
+
+app.get("/api/v1/linkedin", (req, res) => {
+	const instaSocial = {
+		username: "aakashrawatlinkedinpage",
+		followers: 69,
+		follows: 70,
+		date: format.asString("dd[MM] -- hh:mm:ss", new Date()),
+	};
+
+	res.status(200).json({ instaSocial });
+});
+
+app.get("/api/v1/:token", (req, res) => {
+	console.log(req.params.token);
+	res.status(200).json({ param: req.params.token });
+});
+
+app.listen(PORT, () => {
+	console.log(`server is running at PORT ${PORT}`);
+});
