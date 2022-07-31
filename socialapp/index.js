7bb0f5b742e42fefdf74cc018ea9618984961252
Aakash Rawat <01tripster@gmail.com>
2022-07-31 22:27:28 +0530

    added documentation for the project

--- socialapp/index.js
+++ socialapp/index.js
@@ -3,6 +3,13 @@ const format = require("date-format");
 
 const app = express();
 
+//Swagger docs related
+const swaggerUi = require("swagger-ui-express");
+const YAML = require("yamljs");
+const swaggerDocument = YAML.load("./swagger.yaml");
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
 const PORT = process.env.PORT || 4000;
 
 app.get("/", (req, res) => {
