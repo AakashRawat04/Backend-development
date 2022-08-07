60e14d9639284cd73326f3ae409c90aaa1b1e383
Aakash Rawat <01tripster@gmail.com>
2022-08-08 02:03:21 +0530

    swagger docs completed

--- mydocslco/index.js
+++ mydocslco/index.js
@@ -64,6 +64,7 @@ app.get("/api/v1/coursequery", (req, res) => {
 });
 
 app.post("/api/v1/courseupload", (req, res) => {
+	console.log(req.headers);
 	const file = req.files.file;
 	let path = __dirname + "/images/" + Date.now() + ".jpg";
 
