237dcdc4d11e85cebfd3b57ce5f34efe5b2a08f5
Aakash Rawat <01tripster@gmail.com>
2022-08-07 18:32:07 +0530

    v12 sec3 managing request body in swagger

--- mydocslco/index.js
+++ mydocslco/index.js
@@ -7,9 +7,50 @@ const YAML = require("yamljs");
 const swaggerDocument = YAML.load("./swagger.yaml");
 
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use(express.json());
+
+let courses = [
+	{
+		id: "11",
+		name: "Learn ReactJs",
+		price: 299,
+	},
+	{
+		id: "22",
+		name: "Learn Angular",
+		price: 399,
+	},
+	{
+		id: "33",
+		name: "Learn Django",
+		price: 499,
+	},
+];
 
 app.get("/", (req, res) => {
 	res.send("hello from ash!");
 });
 
+app.get("/api/v1/lco", (req, res) => {
+	res.send("hello from lco docs");
+});
+
+app.get("/api/v1/lcoobject", (req, res) => {
+	res.send({ id: "55", name: "Learn Backend", price: 699 });
+});
+
+app.get("/api/v1/courses", (req, res) => {
+	res.send(courses);
+});
+
+app.get("/api/v1/mycourse/:courseId", (req, res) => {
+	const myCourse = courses.find((course) => course.id === req.params.courseId);
+	res.send(myCourse);
+});
+
+app.post("/api/v1/addCourse", (req, res) => {
+	console.log(req.body);
+	courses.push(req.body);
+	res.send(true);
+});
 app.listen(4000, () => console.log("the server is running at port 4000"));
