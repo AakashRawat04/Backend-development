0d55979d6c88306e05a378a844a3675f8cbfd8b4
Aakash Rawat <01tripster@gmail.com>
2022-08-09 19:30:18 +0530

    database connection in auth system

--- authSystem/app.js
+++ authSystem/app.js
@@ -1,4 +1,5 @@
 require("dotenv").config();
+require("./config/database").connect();
 const express = require("express");
 
 const User = require("./model/user");
@@ -10,14 +11,14 @@ app.get("/", (req, res) => {
 	res.send("<h1>Hello from auth system</h1>");
 });
 
-app.post("/register", (req, res) => {
+app.post("/register", async (req, res) => {
 	const { firstname, lastname, email, password } = req.body;
 
 	if (!(email && password && firstname && lastname)) {
 		res.status(400).send("All fields are required!");
 	}
 
-	const existingUser = User.findOne({ email });
+	const existingUser = await User.findOne({ email }); // PROMISE
 
 	if (existingUser) {
 		res.status(401).send("User already exists!");
