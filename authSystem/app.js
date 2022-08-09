b35f63412ec1cd2578c2198fccf3624b3f4cf183
Aakash Rawat <01tripster@gmail.com>
2022-08-09 21:19:39 +0530

    what is JWT and creating token

--- authSystem/app.js
+++ authSystem/app.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 require("./config/database").connect();
 const express = require("express");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 const User = require("./model/user");
 
@@ -13,26 +14,44 @@ app.get("/", (req, res) => {
 });
 
 app.post("/register", async (req, res) => {
-	const { firstname, lastname, email, password } = req.body;
+	try {
+		const { firstname, lastname, email, password } = req.body;
 
-	if (!(email && password && firstname && lastname)) {
-		res.status(400).send("All fields are required!");
+		if (!(email && password && firstname && lastname)) {
+			res.status(400).send("All fields are required!");
+		}
+
+		const existingUser = await User.findOne({ email }); // PROMISE
+
+		if (existingUser) {
+			res.status(401).send("User already exists!");
+		}
+
+		const myEncPassword = await bcrypt.hash(password, 10);
+
+		const user = await User.create({
+			firstname,
+			lastname,
+			email: email.toLowerCase(),
+			password: myEncPassword,
+		});
+
+		//token
+		const token = jwt.sign(
+			{ user_id: user._id, email },
+			process.env.SECRET_KEY,
+			{
+				expiresIn: "2h",
+			}
+		);
+		user.token = token;
+		//update or not in DB
+		//TODO: handle password situation
+
+		res.status(201).json(user);
+	} catch (error) {
+		console.log(error);
 	}
-
-	const existingUser = await User.findOne({ email }); // PROMISE
-
-	if (existingUser) {
-		res.status(401).send("User already exists!");
-	}
-
-	const myEncPassword = await bcrypt.hash(password, 10);
-
-	const user = await User.create({
-		firstname,
-		lastname,
-		email: email.toLowerCase(),
-		password: myEncPassword,
-	});
 });
 
 module.exports = app;
