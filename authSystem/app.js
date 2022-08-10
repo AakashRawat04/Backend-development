21be5acb460c44993c6940f4d94271c4d8924139
Aakash Rawat <01tripster@gmail.com>
2022-08-10 16:40:22 +0530

    login flow for auth app (/login route)

--- authSystem/app.js
+++ authSystem/app.js
@@ -50,10 +50,43 @@ app.post("/register", async (req, res) => {
 		//TODO: handle password situation -> completed
 		user.password = undefined;
 
+		// send token or send just success yes and redirect - choice
 		res.status(201).json(user);
 	} catch (error) {
 		console.log(error);
 	}
 });
 
+app.post("/login", async (req, res) => {
+	try {
+		const { email, password } = req.body;
+
+		if (!(email && password)) {
+			res.status(400).send("Field is missing");
+		}
+
+		const user = await User.findOne({ email });
+
+		// if(!user){
+		// 	res.status(400).send("You are not registered in our app")
+		// }
+
+		if (user && (await bcrypt.compare(password, user.password))) {
+			const token = jwt.sign(
+				{ user_id: user._id, email },
+				process.env.SECRET_KEY,
+				{
+					expiresIn: "2h",
+				}
+			);
+			user.token = token;
+			user.password = undefined;
+			res.status(200).json(user);
+		}
+
+		res.status(400).send("email or password is incorrect");
+	} catch (error) {
+		console.log(error);
+	}
+});
 module.exports = app;
