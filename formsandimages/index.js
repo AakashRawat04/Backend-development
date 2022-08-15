b97f573039a2370be642e9d3351912a084cdf20a
Aakash Rawat <01tripster@gmail.com>
2022-08-15 19:59:32 +0530

    handling images in forms

--- formsandimages/index.js
+++ formsandimages/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const fileupload = require("express-fileupload");
 
 const app = express();
 
@@ -7,10 +8,23 @@ app.set("view engine", "ejs");
 // middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(
+	fileupload({
+		useTempFiles: true,
+		tempFileDir: "/tmp/",
+	})
+);
 
 app.get("/myget", (req, res) => {
 	console.log(req.body);
 
+	// res.send(req.query);
+	res.send(req.body);
+});
+
+app.post("/mypost", (req, res) => {
+	console.log(req.body);
+	console.log(req.files);
 	// res.send(req.query);
 	res.send(req.body);
 });
