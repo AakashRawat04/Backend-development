07f4bd79a17d532c45b5a8397595676af965639f
Aakash Rawat <01tripster@gmail.com>
2022-08-15 19:28:23 +0530

    biggest confusion in front end forms

--- formsandimages/index.js
+++ formsandimages/index.js
@@ -2,6 +2,8 @@ const express = require("express");
 
 const app = express();
 
+app.set("view engine", "ejs");
+
 // middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -9,7 +11,16 @@ app.use(express.urlencoded({ extended: true }));
 app.get("/myget", (req, res) => {
 	console.log(req.body);
 
+	// res.send(req.query);
 	res.send(req.body);
+});
+
+app.get("/mygetform", (req, res) => {
+	res.render("getform");
+});
+
+app.get("/mypostform", (req, res) => {
+	res.render("postform");
 });
 
 app.listen(4000, () => console.log(`Server is running at port 4000...`));
