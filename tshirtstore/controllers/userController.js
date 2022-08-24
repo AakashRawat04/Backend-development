cac46feb6da9900b5d5d67d3d101fa6ce091e633
Aakash Rawat <01tripster@gmail.com>
2022-08-24 15:14:49 +0530

    testing photo upload and user signup

--- tshirtstore/controllers/userController.js
+++ tshirtstore/controllers/userController.js
@@ -10,7 +10,7 @@ exports.signup = BigPromise(async (req, res, next) => {
 
 	if (req.files) {
 		let file = req.files.photo;
-		result = await cloudinary.v2.uploader.upload(file, {
+		result = await cloudinary.v2.uploader.upload(file.tempFilePath, {
 			folder: "users",
 			width: 150,
 			crop: "scale",
