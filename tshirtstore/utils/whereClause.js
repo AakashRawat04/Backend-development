77142e3fa55fe662f354992e5cef890fa81f4512
Aakash Rawat <01tripster@gmail.com>
2022-08-27 14:02:34 +0530

    aggregate filer in where clause

--- tshirtstore/utils/whereClause.js
+++ tshirtstore/utils/whereClause.js
@@ -1,7 +1,7 @@
 // base - Product.find()
 // base - Product.find(email: {"ash@lco.dev"})
 
-//bigQ = search=coder&page=2&category=shortsleeves&rating[gte]=4&price[lte]=999&price[gte]=199
+//bigQ = search=coder&page=2&category=shortsleeves&rating[gte]=4&price[lte]=999&price[gte]=199&limit=5
 
 class WhereClause {
 	constructor(base, bigQ) {
@@ -21,6 +21,26 @@ class WhereClause {
 
 		this.base = this.base.find({ ...searchWord });
 		return this;
+	}
+
+	filter() {
+		const copyQ = { ...this.bigQ };
+
+		delete copyQ["search"];
+		delete copyQ["limit"];
+		delete copyQ["page"];
+
+		//convert bigQ into a string => copyQ
+		let stringOfCopyQ = JSON.stringify(copyQ);
+
+		stringOfCopyQ = stringOfCopyQ.replace(
+			/\b(gte|lte|gt|lt)\b/g,
+			(m) => `$${m}`
+		);
+
+		const jsonOfCopyQ = JSON.parse(stringOfCopyQ);
+
+		this.base = this.base.find(jsonOfCopyQ);
 	}
 
 	pager(resultPerPage) {
