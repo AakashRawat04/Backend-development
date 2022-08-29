be40e734d7951ed06ab6f5705f9dbccc726258fe
Aakash Rawat <01tripster@gmail.com>
2022-08-29 08:55:02 +0530

    razorpay payments and order

--- tshirtstore/controllers/paymentController.js
+++ tshirtstore/controllers/paymentController.js
@@ -18,7 +18,33 @@ exports.captureStripePayment = BigPromise(async (req, res, next) => {
 
 	res.status(200).json({
 		success: true,
+		amount: req.body.amount,
 		client_secret: paymentIntent.client_secret,
 		//you can optionally send id as well
+	});
+});
+
+exports.sendRazorpayKey = BigPromise(async (req, res, next) => {
+	res.status(200).json({
+		stripeKey: process.env.RAZORPAY_API_KEY,
+	});
+});
+
+exports.captureRazorpayPayment = BigPromise(async (req, res, next) => {
+	var instance = new Razorpay({
+		key_id: process.env.RAZORPAY_API_KEY,
+		key_secret: process.env.RAZORPAY_SECRET,
+	});
+
+	var options = {
+		amount: req.body.amount, // amount in the smallest currency unit
+		currency: "INR",
+	};
+	const myOrder = await instance.orders.create(options);
+
+	res.status(200).json({
+		success: true,
+		amount: req.body.amount,
+		order: myOrder,
 	});
 });
