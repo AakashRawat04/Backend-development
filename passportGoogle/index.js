7aa5ccb2de559da1be631dab5bbb62024c0ef954
Aakash Rawat <01tripster@gmail.com>
2022-08-30 14:48:23 +0530

    preparing routes for login

--- passportGoogle/index.js
+++ passportGoogle/index.js
@@ -1,6 +1,16 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const auth = require("./routes/auth");
 const app = express();
+
+//connect to the DB
+mongoose.connect("mongodb://127.0.0.1:27017/passport", () =>
+	console.log("DB connected")
+);
+
 app.set("view engine", "ejs");
+
+app.use("/auth", auth);
 app.get("/", (req, res) => {
 	res.render("home");
 });
